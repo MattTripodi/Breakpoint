f9df58e745cea42ae702be668f640314a4c5b49c
Matt Tripodi <mtripodi1027@gmail.com>
2017-10-03 15:24:05 -0400

    FeedVC tableView now pupulating with messages

---
+++ Breakpoint/Model/Message.swift
@@ -0,0 +1,26 @@
+//
+//  Message.swift
+//  Breakpoint
+//
+//  Created by Matt Tripodi on 10/3/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+	private var _content: String
+	private var _senderId: String
+	
+	var content: String {
+		return _content
+	}
+	var senderId: String {
+		return _senderId
+	}
+	
+	init(content: String, senderId: String) {
+		self._content = content
+		self._senderId = senderId
+	}
+}
---
+++ Breakpoint/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Breakpoint
+//
+//  Created by Matt Tripodi on 10/3/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import Foundation
+
+// Cell Identifiers
+let FEED_CELL = "feedCell"
